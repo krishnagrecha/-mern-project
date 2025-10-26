baa0e0accdf09099cd2c2abe86a0a034496de97a
krishuuu <70624893+KRIKU05@users.noreply.github.com>
2025-10-27 03:22:09 +0530

    done with adminslice

---
+++ frontend/src/redux/slices/adminProductSlice.js
@@ -0,0 +1,18 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const API_URL = `${import.meta.env.VITE_BACKEND_URL}`;
+const USER_TOKEN = `Bearer ${localStorage.getItem("userToken")}`;
+
+// async thunk to fetch admin products
+export const fetchAdminProducts = createAsyncThunk(
+  "adminProducts/fetchProducts",
+  async () => {
+    const response = await axios.get(`${API_URL}/api/admin/products`, {
+      headers: {
+        Authorization: USER_TOKEN,
+      },
+    });
+    return response.data;
+  }
+);
